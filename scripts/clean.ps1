06008019b26eb82bf32a94e6fcd4ba165c509540
Krindendo work <lollipopfunkyx9@gmail.com>
2024-02-02 21:12:53 +0100

    chore: remove all unnecessary packages.

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,3 +1,5 @@
 Write-Host "PowerShell start working"
 
 Get-ChildItem -Path . -Filter "node_modules" -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem -Path . -Filter ".nuxt" -Recurse | Remove-Item -Force -Recurse
+Get-ChildItem -Path . -Filter ".turbo" -Recurse | Remove-Item -Force -Recurse
